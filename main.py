33bce2375c073dd6b76e3234441ca93d16ecc7c0
Maria Caldeira <mariagcaldeira99@gmail.com>
2020-05-27 18:05:16 +0100

    imports working

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ Created on Wed May 27 17:57:40 2020
 
 @author: Maria Caldeira
 """
-import FoldingBlocks
-import Agent
+
+
+from foldingblocks import FoldingBlocks
+from agent import Agent
 
 game = FoldingBlocks()
 agent = Agent(game)
