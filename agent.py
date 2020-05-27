59b5cd00a92076799db46fdddc6d851328d30e45
Maria Caldeira <mariagcaldeira99@gmail.com>
2020-05-27 02:01:15 +0100

    more functions in agent file

--- agent.py
+++ agent.py
@@ -4,13 +4,13 @@ Created on Wed May 27 01:08:53 2020
 
 @author: Maria Caldeira
 """
-
+import numpy as np
+import json
 import sys
 
 #class Agent(object):
 class Agent():
-    """Agent is the reinforcement learning agent that learns optimal state action pairs."""
-    def __init__(self, game, qtable=dict(), player='X', learning_rate=5e-1, discount=9e-1, epsilon=5e-1):
+    def __init__(self, game, q_table=dict(), player='X', learning_rate=5e-1, discount=9e-1, epsilon=5e-1):
         """Initialize agent with properties
         - qtable is json table with Q values Q(s,a)
         - game is reference to game being played
@@ -20,8 +20,23 @@ class Agent():
         - epsilon is probability of exploration in epsilon greedy strategy
         """
         self.game = game
-        self.qtable = qtable
+        self.q_table = q_table
         self.player = player
         self.learning_rate = learning_rate
         self.discount = discount
         self.epsilon = epsilon
+        
+    def qvalue(self, state):
+        if state not in self.q_table:
+            # Initialize Q-value at 0
+            self.q_table[state] = 0.0
+        return self.q_table[state]
+
+    def argmax(self, values):
+        """Returns index of max value."""
+        vmax = np.max(values)
+        max_indices = []
+        for i, v in enumerate(values):
+            if v == vmax:
+                max_indices.append(i)
+        return np.random.choice(max_indices)
